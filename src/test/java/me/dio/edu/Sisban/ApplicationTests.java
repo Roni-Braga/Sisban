683c29140442162b52be1b0b09d45b49fe9179b7
Roni Braga <sousaronielson@gmail.com>
2024-06-30 19:39:12 -0300

    estrutura do projeto

---
+++ src/test/java/me/dio/edu/Sisban/ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.edu.Sisban;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
